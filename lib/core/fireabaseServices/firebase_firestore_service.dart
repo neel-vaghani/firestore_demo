15a618de4ecf024d92bb76391e5f359828c1c8b8
Neel Vaghani <neel.vaghani@joflee.com>
2022-07-15 17:25:06 +0530

    change upload data

--- lib/core/fireabaseServices/firebase_firestore_service.dart
+++ lib/core/fireabaseServices/firebase_firestore_service.dart
@@ -1,14 +1,16 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FireBaseFireStoreService {
-  static final db = FirebaseFirestore.instance;
+  static FirebaseFirestore db = FirebaseFirestore.instance;
   static void getStarted_addData() async {
     // [START get_started_add_data_1]
     // Create a new user with a first and last name
     final user = <String, dynamic>{
-      "first": "Ada",
-      "last": "Lovelace",
-      "born": 1815
+      "first": "Abhishek",
+      "last": "Patil",
+      "born": 2000
     };
 
     // Add a new document with a generated ID
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/fireabaseServices/firebase_firestore_service.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'Dev',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
